883ee75a7663111e260db9594142e2626739890e
0BlackSmoke0 <bertucci.henrique@gmail.com>
2020-10-22 13:34:23 -0300

    Teste api

--- MONITORAMENTO/pythonHM_coldStock/services/OpenHM.py
+++ MONITORAMENTO/pythonHM_coldStock/services/OpenHM.py
@@ -45,6 +45,7 @@ class hardwareMonitor:
         Cpu = []
         Clocks = []
         temperatures = []
+        
         data = self.data
         for i in data['Children']:
             # info['Desktop'] = i['Text']
@@ -56,19 +57,19 @@ class hardwareMonitor:
                         if cpu_metrica['Text'] == 'Clocks':
                             for clock in cpu_metrica['Children']:
                                 if clock['Text'].find('CPU')>= 0:
-                                    # conversao(clock['Value'], 'MHz')
                                     Clocks.append(clock['Value'])
+                                    total += conversao(clock['Value'], 'MHz')
+                                    info['CPU'] = round((total / len(Clocks)/1000), 2)
                                     
                         #Temperature
                         if cpu_metrica['Text'] == "Temperatures":
                             for temperature in cpu_metrica['Children']:
                                 if temperature['Text'].find('CPU')>= 0:
-                                    # conversao(temperature['Value'], '°C')
                                     temperatures.append(temperature['Value'])
-                                    
-                        Cpu = [Clocks,temperatures]
-                # #RAM
-                if desktop['Text'].find('Generic Memory') >= 0:
+                                    total += conversao(temperature['Value'], '°C')
+                                    info['TEMPERATURA'] = round((total / len(temperatures), 2)
+                    
+                if desktop['Text'].find('Generic Memory') >=0:
                     #Load
                     for Memory in desktop['Children']:
                                         
@@ -76,10 +77,9 @@ class hardwareMonitor:
                             for ram in Memory['Children']:
                                 #Memoria usada
                                 if(ram['Text'] == 'Used Memory'):
-                                    # conversao(ram['Value'], ' GB')
-                                    ram['Value'] = ram['Value'].replace(' GB', '')
-                                    ram['Value'] = ram['Value'].replace(',', '.')
-                                    info['RAM'] = ram['Value']
+                                    # ram['Value'] = ram['Value'].replace(' GB', '')
+                                    # ram['Value'] = ram['Value'].replace(',', '.')
+                                    info['RAM'] = conversao(ram['Value'], ' GB')
                                 #Memoria Livre
                 # if desktop['Text'].find('Generic Hard Disk') >= 0:
                 #     #Load
@@ -90,30 +90,31 @@ class hardwareMonitor:
                 #                 #Memoria usada
                 #                 if(percent['Text'] == 'Used Space'):
                 #                     info['DISCO'] = percent['Value']
-                
-        # def conversao(componente, metrica):
-        #     componente = componente.replace(metrica, '')
-        #     componente = componente.replace(',', '.')
-        #     componente = float(componente)
 
-        contadorTemp = 0
-        contadorFreq = 0
+        total = 0.0     
+        def conversao(componente, metrica):
+            componente = componente.replace(metrica, '')
+            componente = componente.replace(',', '.')
+            componente = float(componente)
+            return componente
+
+        # contadorTemp = 0
+        # contadorFreq = 0
         
-        total = 0
+        
+        # while contadorTemp < len(Cpu[1]):
+        #     conversao = Cpu[1][contadorTemp].replace('°C', '')
+        #     conversao = conversao.replace(',', '.')
+        #     total += float(conversao)
+        #     contadorTemp += 1
+        # info['TEMPERATURA'] = round((total / len(Cpu[1])), 2)    
 
-        while contadorTemp < len(Cpu[1]):
-            conversao = Cpu[1][contadorTemp].replace('°C', '')
-            conversao = conversao.replace(',', '.')
-            total += float(conversao)
-            contadorTemp += 1
-        info['TEMPERATURA'] = round((total / len(Cpu[1])), 2)    
-
-        while contadorFreq < len(Cpu[0]):
-            conversao = Cpu[0][contadorFreq].replace('MHz', '')
-            conversao = conversao.replace(',', '.')
-            total += float(conversao)
-            contadorFreq += 1
-        info['CPU'] = round((total / len(Cpu[0])/1000), 2)
+        # while contadorFreq < len(Cpu[0]):
+        #     conversao = Cpu[0][contadorFreq].replace('MHz', '')
+        #     conversao = conversao.replace(',', '.')
+        #     total += float(conversao)
+        #     contadorFreq += 1
+        # info['CPU'] = round((total / len(Cpu[0])/1000), 2)
 
         
         novoInfo =[info['CPU'], info['RAM'], info['TEMPERATURA']] 
