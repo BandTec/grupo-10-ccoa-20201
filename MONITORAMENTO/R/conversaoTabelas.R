fd6bc9f9643dde0e702abb13f11a48b381d3e622
Gabriel Gameiro <gabriel.perez@bandtec.com.br>
2020-12-04 12:56:08 -0300

    novos scripts R

--- MONITORAMENTO/R/conversaoTabelas.R
+++ MONITORAMENTO/R/conversaoTabelas.R
@@ -11,108 +11,63 @@ DATAS = valorInicial
 
 dataAtual = ''
 maiorQtdItens = 0
-contadoComSucesso = FALSE
 
-#Comecando Looping que ira olhar linha por linha do BD
-x = 1
-while (x <= nrow(tabela)) {
+#Pegando quais componentes vieram do BD
+componentes = c()
+componenteAtual = ''
+for (linha in 1:nrow(tabela)) {
+  componenteAtual <- tabela[linha,]$nomeComponente
+  if(!is.element(componenteAtual, componentes)){
+    componentes <- c(componentes, componenteAtual)
+  }
+}
 
+#Matriz sera um esboco do dataset que iremos criar no final
+matriz <- matrix(ncol = length(componentes)+1)
+colnames(matriz) <- append(componentes, "dataHora")
+
+linhaMatriz = 1
+
+for (linhaTabela in 1:nrow(tabela)) {
   #Pegando Linha x do export do BD
   #Os campos que estao sendo pegos sao desde o 1 ate o N° total de colunas que tiver [1:final]
-  linhaAtual = tabela[x, 1:ncol(tabela)]
+  linhaAtual = tabela[linhaTabela,]
   
-  if(dataAtual != linhaAtual$dataHora){
-    dataAtual = linhaAtual$dataHora
-    DATAS[length(DATAS)+1] = dataAtual
+  dataAtual = linhaAtual$dataHora
+  if(is.na(matriz[linhaMatriz, "dataHora"])){
+    #Setando a data caso nao tenha sido feito ainda
+    matriz[linhaMatriz, "dataHora"] = dataAtual
+  }
+  else{
+    #Se a data nao e null, e diferente da atual, add linha
+    if(matriz[linhaMatriz, "dataHora"] != dataAtual){
+      print('Linha adicionada')
+      matriz <- rbind(matriz, rep(NA, length(componentes)+1))
+      linhaMatriz = linhaMatriz + 1
+    }
   }
   
-  #Identifica a qual componente a linha esta se referindo
-  if(linhaAtual$nomeComponente == "CPU"){
-    #Entra no vetor daquele componente e adiciona mais um valor.
-    CPU[length(CPU)+1] = linhaAtual$valor
-  }
-  else if (linhaAtual$nomeComponente == "RAM")
-    RAM[length(RAM)+1] = linhaAtual$valor
-    
-  else if (linhaAtual$nomeComponente == "Disco")
-    DISCO[length(DISCO)+1] = linhaAtual$valor
+  #Valor sendo adicionado na matriz
+  matriz[linhaMatriz, linhaAtual$nomeComponente] <- linhaAtual$valor
   
-  else if (linhaAtual$nomeComponente == "conexaoU")
-    CONEXAOU[length(CONEXAOU)+1] = linhaAtual$valor
-  
-  else if (linhaAtual$nomeComponente == "conexaoD")
-    CONEXAOD[length(CONEXAOD)+1] = linhaAtual$valor
-  
-  else if (linhaAtual$nomeComponente == "temperatura")
-    TEMP[length(TEMP)+1] = linhaAtual$valor
-  
-  #Passando para a proxima linha
-  x <- x + 1
+  x = x+ 1
 }
-
-#Conto quantos de Cada Componente veio do BD
-#Isso pois para criar uma nova tabela eu preciso saber quantas linhas ela tera
-#Atraves do n° presente no maiorQtdDeItens dimensionamos a qtd de linhas em nossa tabela
-
-verificarQtdItens <- function(componente){
-  maiorItens = maiorQtdItens
-  if(length(componente) > maiorItens){
-  maiorItens = length(componente)
-}
-  return(maiorItens)
-}
-
-maiorQtdItens = verificarQtdItens(CPU)
-maiorQtdItens = verificarQtdItens(RAM)
-maiorQtdItens = verificarQtdItens(DISCO)
-maiorQtdItens = verificarQtdItens(CONEXAOD)
-maiorQtdItens = verificarQtdItens(CONEXAOU)
-maiorQtdItens = verificarQtdItens(TEMP)
-
-
-adicionarVazio <- function(componente) {
-  if(length(componente) != 0){
-    while(length(componente) < maiorQtdItens){
-      componente[length(componente) + 1] = 'Vazio'
-    } 
-  }
-  return(componente)
-}
-
-CPU <- adicionarVazio(CPU)
-RAM <- adicionarVazio(RAM)
-DISCO <- adicionarVazio(DISCO)
-CONEXAOD <- adicionarVazio(CONEXAOD)
-CONEXAOU <- adicionarVazio(CONEXAOU)
-TEMP <- adicionarVazio(TEMP)
-
-
-
-#Crio uma nova tabela
-novaTabela = data.frame(ID = 1:maiorQtdItens)
 
 adicionarColunas <- function(componente) {
-  if(is.null(componente) == FALSE)
+  if(all(is.na(componente)) == FALSE)
     return(componente)
 }
 
-#Verifico se eu coletei um determinado componente.
-#Se o vetor do componente esta vazio, passa reto
-#Se nao, ele adiciona uma coluna a tabela.
-#Coluna esta que tera os mesmos valores do vetor
+#Crio uma nova tabela
+novaTabela = data.frame(ID = 1:nrow(matriz))
+DATAS <- matriz[1:nrow(matriz) ,"dataHora"]
 
-novaTabela$CPU <- adicionarColunas(CPU)
-novaTabela$RAM <- adicionarColunas(RAM)
-novaTabela$DISCO <- adicionarColunas(DISCO)
-novaTabela$CONEXAOD <- adicionarColunas(CONEXAOD)
-novaTabela$CONEXAOU <- adicionarColunas(CONEXAOU)
-novaTabela$TEMP <- adicionarColunas(TEMP)
-novaTabela$DATA <- DATAS
+novaTabela$CPU <- adicionarColunas( matriz[1:nrow(matriz) ,"CPU"])
+novaTabela$RAM <- adicionarColunas( matriz[1:nrow(matriz) ,"RAM"])
+novaTabela$Disco <- adicionarColunas( matriz[1:nrow(matriz) ,"Disco"])
+novaTabela$ConexaoD <- adicionarColunas( matriz[1:nrow(matriz) ,"conexaoD"])
+novaTabela$ConexaoU <- adicionarColunas( matriz[1:nrow(matriz) ,"conexaoU"])
+novaTabela$Temperatura <- adicionarColunas( matriz[1:nrow(matriz) ,"temperatura"])
+novaTabela$Data <- DATAS
 
-#length(resid(lm(mpg  ~ cyl, exportBD)))
-
-#EXPORTANDO NOVA TABELA
-write.table(
-  tabelaPerfis, file = "exportTabela.csv",
-  sep = ",", col.names = TRUE, row.names = FALSE,
-  fileEncoding = "UTF-8")
+View(novaTabela)
