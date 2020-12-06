ef793a3ccbccb8a7f8ec34c34b152c2bef0b4d2b
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2020-12-06 12:15:32 -0300

    Rede neural salvando inteligencia

--- MONITORAMENTO/Rede neural/teste.py
+++ MONITORAMENTO/Rede neural/teste.py
@@ -1,15 +1,13 @@
 from mlp import MLP
 import pickle
 
-treino_entradas = [0.7,0.5,0.3,0.6,0.8,0.7]
-treino_saidas =  [1]
 redeRecuperada = open('redeSalva.pkl', 'rb')
 
 rede = pickle.load(redeRecuperada)
-rede.treinamento(treino_entradas, treino_saidas, eta=0.1)
+redeRecuperada.close()
+treino_entradas = [[0.7,0.5,0.3,0.6,0.8,0.7]]
 
 for exemplo in treino_entradas:
     #e no fim, imprimimos cada exemplo da entrada e o resultado da RN
     print(exemplo, rede.teste(exemplo))
 
-redeRecuperada.close()
