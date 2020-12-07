761b9de34219e9db1f4c27b171947cb69698cce5
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2020-12-07 16:23:08 -0300

    Rede miolos

--- MONITORAMENTO/Rede neural/teste.py
+++ MONITORAMENTO/Rede neural/teste.py
@@ -5,7 +5,7 @@ redeRecuperada = open('redeSalva.pkl', 'rb')
 
 rede = pickle.load(redeRecuperada)
 redeRecuperada.close()
-treino_entradas = [[0.7,0.5,0.3,0.6,0.8,0.7]]
+treino_entradas = [[0.7,0.5,0.3,0.6,0.9,0.4],[0.0, 0.9, 0, 0.5,0.4,0.7],[0.0,0.0,0.0,0.0,0.0,0.0]]
 
 for exemplo in treino_entradas:
     #e no fim, imprimimos cada exemplo da entrada e o resultado da RN
--- MONITORAMENTO/Rede neural/xor.py
+++ MONITORAMENTO/Rede neural/xor.py
@@ -3,9 +3,9 @@ import pickle
 
 #aqui criamos as entradas
 #[cpu, ram, disco, conexaoU, conexaoD, temp]
-treino_entradas = [[0.7,0.5,0.6,0.3,0.6,0.9], [0.4,0.8,0.4,0.5,0.7,0.8], [0.7,0.2,0.5,0.6,0.8,0.3], [0.6,0.7,0.8,0.4,0.8,0.9], [0.7,0.5,0.3,0.6,0.8,0.7]]
+treino_entradas = [[0.8,0.5,0.2,0.65,0.56,0.48], [0.8,0.5,0.4,0.65,0.3,0.57], [0.7,0.54,0.13,0.25,0.24,0.58], [0.8,0.5,0.13,0.4,0.3,0.48], [0.9,0.3,0.1,0.6,0.56,0.6],[0.78,0.51,0.1,0.30,0.25,0.58]]
 #e o resultado esperado para a saida
-treino_saidas = [[0], [1], [1], [0], [1]]
+treino_saidas = [[1], [1], [1], [0], [0],[0]]
 
 #aqui,criamos a quantidade de neuronios para cada camada, 
 #temos 6 neuronios para entrada, 6 para oculta e um para resultado
