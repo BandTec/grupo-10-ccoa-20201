d138c01d8953fc300c38b290369dcd7e48d49170
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2020-12-10 10:27:10 -0300

    Correlacao pronta

--- MONITORAMENTO/Rede neural/correlation.py
+++ MONITORAMENTO/Rede neural/correlation.py
@@ -12,11 +12,10 @@ b = saida.values.reshape(saida.shape[1],saida.shape[0])
 
 resposta = b
 
-df = pd.DataFrame(columns=['CPU', 'RAM', 'DISCO', 'RESPOSTA'])
+df = pd.DataFrame(columns=['CPU', 'RAM', 'DISCO', 'CONEXAO D', 'CONEXAO U', 'TEMPERATURA', 'RESPOSTA'])
 
 for i in range(len(entrada[0])):    
-     df = df.append({'CPU': entrada[0][i],'RAM': entrada[1][i],'DISCO': entrada[2][i], 'RESPOSTA': resposta[i][0] }, ignore_index=True)
+     df = df.append({'CPU': entrada[0][i],'RAM': entrada[1][i],'DISCO': entrada[2][i], 'CONEXAO D': entrada[3][i], 'CONEXAO U': entrada[4][i], 'TEMPERATURA': entrada[5][i],  'RESPOSTA': resposta[i][0]}, ignore_index=True)
 
-print(df)
-print(df.corr())
+print(round(df.corr(),3))
 
