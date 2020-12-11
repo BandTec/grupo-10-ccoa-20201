deaf7b8add12731890fb062cc67a5cc3935c725a
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2020-12-11 11:27:10 -0300

    Preparacao pra integracao java

--- MONITORAMENTO/Client Java/monitoramento-ColdStock/Rede neural/teste.py
+++ MONITORAMENTO/Client Java/monitoramento-ColdStock/Rede neural/teste.py
@@ -11,10 +11,15 @@ redeRecuperada = open('redeSalva.pkl', 'rb')
 rede = pickle.load(redeRecuperada)
 redeRecuperada.close()
 treino_entradas = [[0.57, 0.42, 0.04, 0.0, 0.0, 0.69],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[0.39, 0.41, 0.04, 0.0, 0.0, 0.69]]
-
+contadorChamados = 0
 for exemplo in treino_entradas:
     resposta = rede.teste(exemplo)
-    print(exemplo,resposta[i])
+    for i in range(len(resposta)):
+        print(exemplo,round(resposta[i],0))
+        if round(resposta[i],1) == 1.0:
+            contadorChamados += 1
+print(contadorChamados)
+
 
 end = timer()
 tempo = end-start
