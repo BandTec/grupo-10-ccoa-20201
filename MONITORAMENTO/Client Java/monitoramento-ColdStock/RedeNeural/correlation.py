8909d5276185ac60fe3d313ecaa1d6d6f22380cb
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2020-12-11 13:13:36 -0300

    Correlacao finalizada

--- MONITORAMENTO/Client Java/monitoramento-ColdStock/RedeNeural/correlation.py
+++ MONITORAMENTO/Client Java/monitoramento-ColdStock/RedeNeural/correlation.py
@@ -1,16 +1,28 @@
 import pandas as pd
 
-entrada = pd.read_csv("./ColetorDadosBD/dados.data", sep=",", header=None)
-saida = pd.read_csv("./ColetorDadosBD/saida.data", sep=",", header=None)
 
-b = saida.values.reshape(saida.shape[1],saida.shape[0])
+class Correlacao:
+     def criarCorrelacao(self):
+          entrada = pd.read_csv("./ColetorDadosBD/dados.data", sep=",", header=None)
+          saida = pd.read_csv("./ColetorDadosBD/saida.data", sep=",", header=None)
 
-resposta = b
+          b = saida.values.reshape(saida.shape[1],saida.shape[0])
+          resposta = b
 
-df = pd.DataFrame(columns=['CPU', 'RAM', 'DISCO', 'CONEXAO D', 'CONEXAO U', 'TEMPERATURA', 'RESPOSTA'])
+          df = pd.DataFrame(columns=['CPU', 'RAM', 'DISCO', 'CONEXAO D', 'CONEXAO U', 'TEMPERATURA', 'RESPOSTA'])
 
-for i in range(len(entrada[0])):    
-     df = df.append({'CPU': entrada[0][i],'RAM': entrada[1][i],'DISCO': entrada[2][i], 'CONEXAO D': entrada[3][i], 'CONEXAO U': entrada[4][i], 'TEMPERATURA': entrada[5][i],  'RESPOSTA': resposta[i][0]}, ignore_index=True)
+          for i in range(len(entrada[0])):    
+               df = df.append({'CPU': entrada[0][i],'RAM': entrada[1][i],'DISCO': entrada[2][i], 'CONEXAO D': entrada[3][i], 'CONEXAO U': entrada[4][i], 'TEMPERATURA': entrada[5][i],  'RESPOSTA': resposta[i][0]}, ignore_index=True)
 
-print(round(df.corr(),3))
+          correlacao = round(df.corr(),3)
+          df = correlacao
+
+          respostas = []
+
+          for i in range(6):
+               respostas.append(df.iloc[6][i])
+          return respostas
+
+
+
 
--- MONITORAMENTO/Client Java/monitoramento-ColdStock/RedeNeural/teste.py
+++ MONITORAMENTO/Client Java/monitoramento-ColdStock/RedeNeural/teste.py
@@ -3,26 +3,35 @@ import pickle
 from timeit import default_timer as timer
 import time
 
-# def teste(valores):
-start = timer()
+class Teste():
+    def recuperarRede(self):
+        redeRecuperada = open('redeSalva.pkl', 'rb')
+        rede = pickle.load(redeRecuperada)
+        redeRecuperada.close()
+        return rede
 
-redeRecuperada = open('redeSalva.pkl', 'rb')
+    def realizarTeste(self, valores):
+        start = timer()
+        treino_entradas = valores #[[0.57, 0.42, 0.04, 0.0, 0.0, 0.69],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[0.39, 0.41, 0.04, 0.0, 0.0, 0.69]]
+        contadorChamados = 0
+        for exemplo in treino_entradas:
+            resposta = self.recuperarRede().teste(exemplo)
+            for i in range(len(resposta)):
+                print(exemplo,round(resposta[i],0))
+                if round(resposta[i],1) == 1.0:
+                    contadorChamados += 1
+        print(contadorChamados)
 
-rede = pickle.load(redeRecuperada)
-redeRecuperada.close()
-treino_entradas = [[0.57, 0.42, 0.04, 0.0, 0.0, 0.69],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[0.39, 0.41, 0.04, 0.0, 0.0, 0.69]]
-contadorChamados = 0
-for exemplo in treino_entradas:
-    resposta = rede.teste(exemplo)
-    for i in range(len(resposta)):
-        print(exemplo,round(resposta[i],0))
-        if round(resposta[i],1) == 1.0:
-            contadorChamados += 1
-print(contadorChamados)
+        end = timer()
+        tempo = end-start
+        print("Tempo de processamento:", round(tempo,8),"segundos")
+        return contadorChamados
 
 
-end = timer()
-tempo = end-start
-print("Tempo de processamento:", round(tempo,8),"segundos")
+
+
+
+
+
 
 
