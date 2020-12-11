79dab5c6a51b1633bb2461e45115013d59239f03
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2020-12-11 11:01:55 -0300

    Reorganizando pasta

--- MONITORAMENTO/Client Java/monitoramento-ColdStock/Rede neural/teste.py
+++ MONITORAMENTO/Client Java/monitoramento-ColdStock/Rede neural/teste.py
@@ -3,6 +3,7 @@ import pickle
 from timeit import default_timer as timer
 import time
 
+# def teste(valores):
 start = timer()
 
 redeRecuperada = open('redeSalva.pkl', 'rb')
@@ -12,16 +13,11 @@ redeRecuperada.close()
 treino_entradas = [[0.57, 0.42, 0.04, 0.0, 0.0, 0.69],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[0.39, 0.41, 0.04, 0.0, 0.0, 0.69]]
 
 for exemplo in treino_entradas:
-    print(exemplo, rede.teste(exemplo))
+    resposta = rede.teste(exemplo)
+    print(exemplo,resposta[i])
 
 end = timer()
-
 tempo = end-start
-
 print("Tempo de processamento:", round(tempo,8),"segundos")
 
-redeSalva = open('redeSalva.pkl', 'wb')
 
-pickle.dump(rede, redeSalva)
-
-redeSalva.close()
